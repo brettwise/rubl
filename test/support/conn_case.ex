838c2f03c469e9d2a7171710c32ff701092f69ac
Brett Wise <brettdwise@gmail.com>
2016-02-18 15:27:35 -0500

    Integration tests and unit tests on plug completed and passing save for one test on the video controller

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -26,6 +26,7 @@ defmodule Rubl.ConnCase do
       import Ecto.Query, only: [from: 1, from: 2]
 
       import Rubl.Router.Helpers
+      import Rubl.TestHelpers
 
       # The default endpoint for testing
       @endpoint Rubl.Endpoint
---
+++ test/support/test_helpers.ex
@@ -0,0 +1,21 @@
+defmodule Rubl.TestHelpers do
+  alias Rubl.Repo
+
+  def insert_user(attrs \\ %{}) do
+    changes = Dict.merge(%{
+      name: "Some User",
+      username: "user#{Base.encode16(:crypto.rand_bytes(8))}",
+      password: "supersecret",
+    }, attrs)
+
+    %Rubl.User{}
+    |> Rubl.User.registration_changeset(changes)
+    |> Repo.insert!()
+  end
+
+  def insert_video(user, attrs \\ %{}) do
+    user
+    |> Ecto.build_assoc(:videos, attrs)
+    |> Repo.insert!()
+  end
+end
